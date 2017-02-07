9d2de10347246cd2e8c4a85ebc81983defb4ebfd
Nils Zonneveld <nilszonneveld@gmail.com>
2017-02-07 12:00:33 +0100

    Initiële commit

---
+++ src/main/java/nl/nilsz/axon3/account/AxonAccountApplication.java
@@ -0,0 +1,12 @@
+package nl.nilsz.axon3.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonAccountApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/nilsz/axon3/account/AxonAccountApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.nilsz.axon3.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AxonAccountApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
